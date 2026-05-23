6c04215ef43f658636126128511add257dc5ab76
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2026-05-23 19:12:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,61 @@
-console.log(
-"REHAN CAFEVERSE LOADED"
+/* LOADER */
+
+window.addEventListener(
+"load",
+() => {
+
+setTimeout(() => {
+
+document
+.querySelector(".loader")
+.classList.add("hidden");
+
+},2500);
+
+});
+
+/* CURSOR */
+
+const cursor =
+document.querySelector(".cursor");
+
+document.addEventListener(
+"mousemove",
+(e)=>{
+
+cursor.style.left =
+e.clientX + "px";
+
+cursor.style.top =
+e.clientY + "px";
+
+});
+
+/* BUTTON HOVER EFFECT */
+
+const buttons =
+document.querySelectorAll(
+"button,a"
 );
+
+buttons.forEach((item)=>{
+
+item.addEventListener(
+"mouseenter",
+()=>{
+
+cursor.style.transform =
+"translate(-50%,-50%) scale(2)";
+
+});
+
+item.addEventListener(
+"mouseleave",
+()=>{
+
+cursor.style.transform =
+"translate(-50%,-50%) scale(1)";
+
+});
+
+});
