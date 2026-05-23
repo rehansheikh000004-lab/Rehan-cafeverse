f454ded6c4bc1d55825fa99e1775e2891ac1c043
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2026-05-23 19:32:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,19 @@
-/* LOADER */
-
-window.addEventListener(
-"load",
-() => {
+window.addEventListener("load", () => {
 
 setTimeout(() => {
 
-document
-.querySelector(".loader")
+document.querySelector(".loader")
 .classList.add("hidden");
 
 },2500);
 
 });
 
-/* CURSOR */
+const cursor = document.querySelector(".cursor");
 
-const cursor =
-document.querySelector(".cursor");
+document.addEventListener("mousemove",(e)=>{
 
-document.addEventListener(
-"mousemove",
-(e)=>{
-
-cursor.style.left =
-e.clientX + "px";
-
-cursor.style.top =
-e.clientY + "px";
-
-});
-
-/* BUTTON HOVER EFFECT */
-
-const buttons =
-document.querySelectorAll(
-"button,a"
-);
-
-buttons.forEach((item)=>{
-
-item.addEventListener(
-"mouseenter",
-()=>{
-
-cursor.style.transform =
-"translate(-50%,-50%) scale(2)";
-
-});
-
-item.addEventListener(
-"mouseleave",
-()=>{
-
-cursor.style.transform =
-"translate(-50%,-50%) scale(1)";
-
-});
+cursor.style.left = e.clientX + "px";
+cursor.style.top = e.clientY + "px";
 
 });
